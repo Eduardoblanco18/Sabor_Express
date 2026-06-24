1eb014b235a6f46472741a5f9debe4d7a0f56d6c
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-06-24 09:35:29 -0300

    Nona Aula

--- oo-SABOR_EXPRESS/app.py
+++ oo-SABOR_EXPRESS/app.py
@@ -1,14 +1,15 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.alternar_estado()
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Vitória', 9)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor da cidade')
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
